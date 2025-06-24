ce7254e2727f0e7bea4766c1e92b0db63c75a432
Mayank-Pandey-Ji <pandeymayank1099@gmail.com>
2025-06-24 21:14:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import HeroSection from './pages/HeroSection'
+import ProblemStatementSection from './pages/ProblemStatementSection'
+function App() {
+
+  return (
+   <div>
+    <HeroSection/>
+   <ProblemStatementSection/>
+   </div>
+  )
+}
+
+export default App
